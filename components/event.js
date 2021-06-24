5c17cc7573e9e1b3c4946c2f96659a52a56121d8
BBinLiu <binliu@brandeis.edu>
2021-06-24 20:08:33 +0800

    hw3

--- components/event.js
+++ components/event.js
@@ -1,19 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, StyleSheet, Text, TextInput, View, FlatList } from "react-native";
-
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Event = (props) => {
-  const [event, setEvent] = useState({destination:'', time:'', limit:''});
+  const [event, setEvent] = useState({destination:'', time:'', limit:'', people:[]});
   const [people, setPeople] = useState([]);
   const [destination, setDestination] = useState("")
   const [time, setTime] = useState("")
   const [limit, setLimit] = useState("")
   const [name, setName] = useState("")
 
+  useEffect(() => {getData()}
+           ,[])
+
+  const getData = async () => {
+        try {
+          // the '@profile_info' can be any string
+          const jsonValue = await AsyncStorage.getItem('@event')
+          let data = null
+          if (jsonValue!=null) {
+            data = JSON.parse(jsonValue)
+            setEvent(data)
+            console.log('just set Info, Name and Email')
+          } else {
+            console.log('just read a null value from Storage')
+          }
+
+
+        } catch(e) {
+          console.log("error in getData ")
+          console.dir(e)
+          // error reading value
+        }
+  }
+
+  const storeData = async (value) => {
+        try {
+          const jsonValue = JSON.stringify(value)
+          await AsyncStorage.setItem('@event', jsonValue)
+          console.log('just stored '+jsonValue)
+        } catch (e) {
+          console.log("error in storeData ")
+          console.dir(e)
+          // saving error
+        }
+  }
+
+  const clearAll = async () => {
+        try {
+          console.log('in clearData')
+          await AsyncStorage.clear()
+        } catch(e) {
+          console.log("error in clearData ")
+          console.dir(e)
+          // clear error
+        }
+  }
+
   const renderItem = ({item}) => {
     return (
       <View style={{border:'thin solid red'}}>
         <Text>{item.name} </Text>
+
       </View>
     )
   }
@@ -46,25 +94,44 @@ const Event = (props) => {
       />
     </View>
 
+
+    <Button
+          color='blue' title='Save this event'
+          onPress = {() => {
+            let newEvent = {destination: destination, time:time, limit:limit, people:[]}
+            setEvent(newEvent)
+            storeData(newEvent)
+          }}
+      />
+
+    <Text style={styles.text}> Destination: {event.destination}</Text>
+    <Text style={styles.text}> Time: {event.time}</Text>
+    <Text style={styles.text}> Limit: {event.limit}</Text>
+
     <TextInput
-        placeholder="Please enter your name if you wish to join this trip!"
-        style={styles.textinput}
-        onChangeText={text => {setName(text)}}
+          placeholder="Please enter your name if you wish to join this trip!"
+          style={styles.textinput}
+          onChangeText={text => {setName(text)}}
     />
 
     <Button
           color='red' title='Join this trip!'
           onPress = {() => {
-               if (people.length < limit) {
-                 setPeople(people.concat({'name': name, 'date':new Date}))}}}
-      />
+          getData()
+          const newPeople = event.people.concat({'name': name, 'date':new Date})
+          event.people = newPeople
+          storeData(event)
+          getData()}
+          }
+    />
 
     <Text style={styles.text}> These people are in this trip! </Text>
     <FlatList
-        data={people}
+        data={event.people}
         renderItem={renderItem}
-        keyExtractor={item => item.date.toString()}
-      />
+        keyExtractor={item => item.date}
+    />
+
   </View>
       );
     }
@@ -79,14 +146,12 @@ const Event = (props) => {
     },
     textinput:{
       border:'thin solid blue',
-      flex: 1,
-      justifyContent: "center",
       margin:20,
       fontSize:20
     },
     text:{
-      flex:1,
       fontSize:20,
+      margin:20,
     },
     header: {
       fontSize:40,
